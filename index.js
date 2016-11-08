988cd253e74aa9e9bcfb6f780932b2b0b185a24a
Jack Murphy <jmurphy.web@gmail.com>
2016-11-08 14:53:18 +0000

    comment out unnecessary logs

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ console.log(process.execPath);
 
 // watch several files/directories
 fs.watch(__dirname, {}, (eventType, filename) => {
-  console.log('watch event triggered', eventType, filename);
+  // console.log('watch event triggered', eventType, filename)
   // how to re-run the application when a change is detected
-  console.log('killing child process');
+  // console.log('killing child process')
   process.kill(child.pid)
-  console.log('restarting new instance');
+  // console.log('restarting new instance')
   child = spawn(process.execPath, [process.argv[2]])
 })
 
 child = spawn(process.execPath, [process.argv[2]])
 
-child.stdout.on('data', function (data) {
-  console.log('message from the child: ', data.toString())
-})
+// child.stdout.on('data', function (data) {
+//   console.log('message from the child: ', data.toString())
+// })
 
 // stretch goals
 // How to add the startmon command to this directory
